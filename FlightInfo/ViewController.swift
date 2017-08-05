f6072d05771ae29281d32e9410c13568131b0515
Matteo Manferdini <matteo@purecreek.com>
2017-08-05 12:20:17 +0200

    Initial Commit

---
+++ FlightInfo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlightInfo
+//
+//  Created by Matteo Manferdini on 05/08/2017.
+//  Copyright © 2017 Pure Creek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
